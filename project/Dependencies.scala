bf4e3ac7e9c9ce987cf97b330d127d0faa7915f4
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2021-07-21 09:09:40 +0100

    Define Circe and ElasticMQ versions to keep them consistent

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,14 +1,17 @@
 import sbt._
 
 object Dependencies {
+  private val circeVersion = "0.13.0"
+  private val elasticMqVersion = "1.1.1"
+
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.9"
   lazy val typesafe = "com.typesafe" % "config" % "1.4.0"
   lazy val s3Sdk = "software.amazon.awssdk" % "s3" % "2.13.18"
   lazy val lambdaJavaCore = "com.amazonaws" % "aws-lambda-java-core" % "1.2.1"
   lazy val lambdaJavaEvents = "com.amazonaws" % "aws-lambda-java-events" % "3.1.0"
-  lazy val circeCore = "io.circe" %% "circe-core" % "0.13.0"
-  lazy val circeGeneric = "io.circe" %% "circe-generic" % "0.13.0"
-  lazy val circeParser = "io.circe" %% "circe-parser" % "0.13.0"
+  lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
+  lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
+  lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
   lazy val awsUtils =  "uk.gov.nationalarchives.aws.utils" %% "tdr-aws-utils" % "0.1.15"
   lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.19"
   lazy val generatedGraphql =  "uk.gov.nationalarchives" %% "tdr-generated-graphql" % "0.0.60"
@@ -17,6 +20,6 @@ object Dependencies {
   lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
   lazy val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
   lazy val mockito = "org.mockito" %% "mockito-scala" % "1.14.1"
-  lazy val elasticMq = "org.elasticmq" %% "elasticmq-server" % "1.1.1"
-  lazy val elasticMqSqs = "org.elasticmq" %% "elasticmq-rest-sqs" % "1.1.1"
+  lazy val elasticMq = "org.elasticmq" %% "elasticmq-server" % elasticMqVersion
+  lazy val elasticMqSqs = "org.elasticmq" %% "elasticmq-rest-sqs" % elasticMqVersion
 }
