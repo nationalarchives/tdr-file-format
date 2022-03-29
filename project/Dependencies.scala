8fb6dbc65ac9cbe051256647d5c6cea19c404388
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-03-29 10:53:36 +0000

    Update aws-lambda-java-events to 3.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   lazy val typesafe = "com.typesafe" % "config" % "1.4.0"
   lazy val s3Sdk = "software.amazon.awssdk" % "s3" % "2.13.18"
   lazy val lambdaJavaCore = "com.amazonaws" % "aws-lambda-java-core" % "1.2.1"
-  lazy val lambdaJavaEvents = "com.amazonaws" % "aws-lambda-java-events" % "3.1.0"
+  lazy val lambdaJavaEvents = "com.amazonaws" % "aws-lambda-java-events" % "3.1.1"
   lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
   lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
   lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
