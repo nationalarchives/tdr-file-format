c2d6dbb2e5a919591d765f64cf5d2c3c27a6779b
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-05-09 11:40:14 +0100

    Move to GitHub actions

---
+++ .github/scripts/run_ecs_task.py
@@ -0,0 +1,34 @@
+import boto3
+import sys
+
+account_number = sys.argv[1]
+stage = sys.argv[2]
+
+client = boto3.client('ecs')
+ec2_client = boto3.client("ec2")
+
+filtered_security_groups = list(filter(lambda filtered_sg: filtered_sg['GroupName'] == "allow-outbound-https",
+                                       ec2_client.describe_security_groups()['SecurityGroups']))
+security_groups = [security_group['GroupId'] for security_group in filtered_security_groups]
+subnets = [subnet['SubnetId'] for subnet in ec2_client.describe_subnets(Filters=[
+    {
+        'Name': 'tag:Name',
+        'Values': [
+            'tdr-private-subnet-0-' + stage,
+            'tdr-private-subnet-1-' + stage,
+        ]
+    },
+])['Subnets']]
+
+response = client.run_task(
+    cluster="keycloak_update_" + stage,
+    taskDefinition="keycloak-update-" + stage,
+    launchType="FARGATE",
+    platformVersion="1.4.0",
+    networkConfiguration={
+        'awsvpcConfiguration': {
+            'subnets': subnets,
+            'securityGroups': security_groups
+        }
+    }
+)
