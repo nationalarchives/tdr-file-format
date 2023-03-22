378212b0df269fcd81772bd5bd542d690a4df204
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2023-03-22 08:29:28 +0000

    Update mockito-scala to 1.17.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5"
   lazy val logback = "ch.qos.logback" % "logback-classic" % "1.4.6"
   lazy val logstashLogbackEncoder = "net.logstash.logback" % "logstash-logback-encoder" % "7.3"
-  lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.12"
+  lazy val mockito = "org.mockito" %% "mockito-scala" % "1.17.14"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock" % "2.27.2"
   lazy val apacheCommons = "org.apache.commons" % "commons-lang3" % "3.12.0"
   lazy val droidApi = "uk.gov.nationalarchives" % "droid-api" % "6.6.1"
